11efe2aec4bcfb30aa61000ce2d20d2ac0be7bf1
Gilvan Fernandes <gil-f-o@hotmail.com>
2016-12-18 05:07:55 -0200

    Layout da tela de login

---
+++ application/views/login.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>AdminLTE 2 | Log in</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.6 -->
+  <?php echo link_tag('front/bootstrap/css/bootstrap.min.css'); ?>
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Theme style -->
+  <?php echo link_tag('front/dist/css/AdminLTE.min.css'); ?>
+  <!-- iCheck -->
+  <?php echo link_tag('front/plugins/iCheck/square/blue.css'); ?>
+
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+</head>
+<body class="hold-transition login-page">
+<div class="login-box">
+  <div class="login-logo">
+    <a href="#"><b>Reserva de Sala </b> Ditech</a>
+  </div>
+
+  <div class="login-box-body">
+
+      <?php echo validation_errors("<div class=\"alert alert-danger alert-dismissible\">","</div>"); ?>
+      <?php echo form_open('login/validacao'); ?>
+      <div class="form-group has-feedback">
+        <input type="email" class="form-control" placeholder="Email" name="email">
+        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+      </div>
+      <div class="form-group has-feedback">
+        <input type="password" class="form-control" placeholder="Senha" name="senha">
+        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+      </div>
+      <div class="row">
+        <div class="col-xs-4">
+          <button type="submit" class="btn btn-primary btn-block btn-flat">Login</button>
+        </div>
+      </div>
+    </form>
+
+    <a href="#">Esqueceu sua senha?</a><br>
+
+  </div>
+</div>
+
+<!-- jQuery 2.2.3 -->
+<script src="<?php echo base_url()."front/plugins/jQuery/jquery-2.2.3.min.js"; ?>"></script>
+<!-- Bootstrap 3.3.6 -->
+<script src="<?php echo base_url()."front/bootstrap/js/bootstrap.min.js"; ?>"></script>
+<!-- iCheck -->
+<script src="<?php echo base_url()."front/plugins/iCheck/icheck.min.js"; ?>"></script>
+<script>
+  $(function () {
+    $('input').iCheck({
+      checkboxClass: 'icheckbox_square-blue',
+      radioClass: 'iradio_square-blue',
+      increaseArea: '20%' // optional
+    });
+  });
+</script>
+</body>
+</html>
