4fa3d962f5f26f70b1d12358e2d92dd016f5d56b
Gilvan Fernandes <gil-f-o@hotmail.com>
2016-12-18 12:16:12 -0200

    Ajuste de layout da reserva de sala

--- application/views/reserva.php
+++ application/views/reserva.php
@@ -13,9 +13,18 @@
       </h1>
     </section>
 
-    <?php if($this->session->userdata('logado')): ?>
-
     <section class="content">
+
+      <?php if($this->session->userdata('logado')): ?>
+
+      <?php if (!empty($sRetorno)): ?>
+      <div class="alert alert-warning alert-dismissible">
+          <strong><?php echo $sRetorno; ?></strong>
+      </div>
+      <?php endif; ?>
+
+      <?php echo validation_errors("<div class=\"alert alert-danger alert-dismissible\">","</div>"); ?>
+      <?php echo form_open('reserva/criar'); ?>
 
       <div class="box box-default">
 
@@ -24,20 +33,17 @@
             <div class="col-md-6">
               <div class="form-group">
                 <label>Sala</label>
-                <select class="form-control select2" style="width: 100%;">
-                  <option selected="selected">Alabama</option>
-                  <option>Alaska</option>
-                  <option disabled="disabled">California (disabled)</option>
-                  <option>Delaware</option>
-                  <option>Tennessee</option>
-                  <option>Texas</option>
-                  <option>Washington</option>
-                </select>
+                <select name="idsala" class="form-control select2" style="width: 100%;">
+                    <?php foreach ($rsSalas as $sSala): ?>
+                        <option value="<?php echo $sSala->id;?>"><?php echo $sSala->sala;?></option>
+                    <?php endforeach;?>
+                 </select>
               </div>
 
               <div class="form-group">
                 <label>Reservador por</label>
-                <input class="form-control" id="exampleInputEmail1" disabled="disabled" type="text">
+                <input class="form-control" value="<?php echo $this->session->userdata('nome'); ?>" disabled="disabled" type="text">
+                <input name="idusuario" value="<?php echo $this->session->userdata('id'); ?>" hidden="hidden" type="text">
               </div>
 
             </div>
@@ -45,7 +51,7 @@
             <div class="col-md-6">
               <div class="form-group">
                   <label>Assunto da Reserva</label>
-                  <input class="form-control" placeholder="Ex.: Definição de backlog"  type="text">
+                  <input name="assunto" class="form-control" placeholder="Ex.: Definição de backlog"  type="text">
 
               </div>
 
@@ -56,7 +62,7 @@
                   <div class="input-group-addon">
                     <i class="fa fa-calendar"></i>
                   </div>
-                  <input type="text" class="form-control" data-inputmask="'alias': 'dd/mm/yyyy'" data-mask>
+                  <input name="data" type="text" class="form-control" data-inputmask="'alias': 'dd/mm/yyyy'" data-mask>
                 </div>
 
               </div>
@@ -66,7 +72,7 @@
                   <label>Horário da Reserva</label>
 
                   <div class="input-group">
-                    <input type="text" class="form-control timepicker">
+                    <input name="hora" type="text" class="form-control timepicker">
 
                     <div class="input-group-addon">
                       <i class="fa fa-clock-o"></i>
@@ -88,7 +94,87 @@
 
       </div>
 
+      <?php endif; ?>
+
+        <div class="row">
+
+            <?php foreach ($rsSalas as $sSala): ?>
+
+            <div class="col-md-6">
+
+              <div class="nav-tabs-custom">
+                <ul class="nav nav-tabs pull-right">
+                  <li class="active"><a href="#tab_1-1_<?php echo $sSala->id.$sSala->sala;?>" data-toggle="tab" aria-expanded="true">Do dia(<?php echo date('d/m/Y');?>)</a></li>
+                  <li class=""><a href="#tab_2-2_<?php echo $sSala->id.$sSala->sala;?>" data-toggle="tab" aria-expanded="false">P/ Amanhã</a></li>
+                  <li class=""><a href="#tab_3-2_<?php echo $sSala->id.$sSala->sala;?>" data-toggle="tab" aria-expanded="false">Mês <?php echo date('m');?></a></li>
+                  <li class="pull-left header"><?php echo $sSala->sala;?></li>
+                </ul>
+                <div class="tab-content">
+                  <div class="tab-pane active" id="tab_1-1_<?php echo $sSala->id.$sSala->sala;?>">
+
+                      <table class="table table-bordered">
+                        <tbody><tr>
+                          <th>Horário</th>
+                          <th>Motivo da Alocação</th>
+                          <th>Status</th>
+
+                          <th style="width: 75px"></th>
+                        </tr>
+
+                        <?php foreach ($rsSalas as $sSala): ?>
+
+                        <tr>
+                          <td><?php echo $sSala->sala;?></td>
+                          <td><?php echo $sSala->detalhes;?></td>
+                          <td>Agendado</td>
+                          <td>
+                            <a href="<?php echo site_url('sala/deletar/'.$sSala->id); ?>">
+                                <span class="badge bg-red">
+                                    <i class="fa fa-edit"></i>
+                                </span>
+                            </a>
+
+                          </td>
+                        </tr>
+
+                        <?php endforeach;?>
+
+                      </tbody>
+                     </table>
+
+                  </div>
+                  <!-- /.tab-pane -->
+                  <div class="tab-pane" id="tab_2-2_<?php echo $sSala->id.$sSala->sala;?>">
+                    The European languages are members of the same family. Their separate existence is a myth.
+                    For science, music, sport, etc, Europe uses the same vocabulary. The languages only differ
+                    in their grammar, their pronunciation and their most common words. Everyone realizes why a
+                    new common language would be desirable: one could refuse to pay expensive translators. To
+                    achieve this, it would be necessary to have uniform grammar, pronunciation and more common
+                    words. If several languages coalesce, the grammar of the resulting language is more simple
+                    and regular than that of the individual languages.
+                  </div>
+                  <!-- /.tab-pane -->
+                  <div class="tab-pane" id="tab_3-2_<?php echo $sSala->id.$sSala->sala;?>">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                    when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                    It has survived not only five centuries, but also the leap into electronic typesetting,
+                    remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset
+                    sheets containing Lorem Ipsum passages, and more recently with desktop publishing software
+                    like Aldus PageMaker including versions of Lorem Ipsum.
+                  </div>
+
+                </div>
+
+              </div>
+
+            </div>
+
+            <?php endforeach;?>
+
+
+        </div>
+
     </section>
-    <?php endif; ?>
 
   </div>
