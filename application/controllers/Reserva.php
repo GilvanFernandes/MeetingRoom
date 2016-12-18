5845440e8661f4c2060ef558f1767184d45b2cd6
Gilvan Fernandes <gil-f-o@hotmail.com>
2016-12-18 10:32:48 -0200

    Ajuste gerais no controlador da reserva da salas

--- application/controllers/Reserva.php
+++ application/controllers/Reserva.php
@@ -6,26 +6,40 @@ class Reserva extends CI_Controller {
     public function __construct(){
         parent::__construct();
 
-        //if(!$this->session->userdata('id_usuario') || !$this->session->userdata('id_empresa') || !$this->session->userdata('logado')){
-        //    redirect('login');
-        //}
-
-        //$this->load->model('financeiro_model');
+        $this->load->model('sala_model');
     }
 
     public function index(){
 
-
-        // Lista dentro option no campo de pesquisa
-        //$aData['rsPessoas']         = $this->pessoa_model->getPessoa();
+        $aData['rsSalas'] = $this->sala_model->getSala();
 
 		$this->load->view('template_header');
-		$this->load->view('reserva');
+		$this->load->view('reserva', $aData);
 		$this->load->view('template_footer');
 
     }
 
     public function criar(){
+
+        $this->form_validation->set_rules('idsala', 'ID sala', 'required|integer');
+        $this->form_validation->set_rules('idusuario', 'ID usuário', 'required|integer');
+        $this->form_validation->set_rules('assunto', 'assunto', 'required');
+        $this->form_validation->set_rules('data', 'data', 'required');
+        $this->form_validation->set_rules('hora', 'hora', 'required');
+
+        if ($this->form_validation->run() === FALSE){
+
+            $this->index();
+
+        }else{
+
+            if($this->sala_model->setReserva()){
+                $this->index(TRUE);
+            }else{
+                $this->index(FALSE);
+            }
+
+        }
 
     }
 
