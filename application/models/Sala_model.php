291595a5f1bcc9636f85f27cbcf78017e5915923
Gilvan Fernandes <gil-f-o@hotmail.com>
2016-12-18 09:48:19 -0200

    Ajustado diversas funções envolvendo model da sala

--- application/models/Sala_model.php
+++ application/models/Sala_model.php
@@ -5,23 +5,26 @@ class Sala_model extends CI_Model {
 
     }
 
-    public function getSala(){
+    public function getSala($iIdSala = NULL){
 
-        $sSQL  ="     SELECT *         ";
-        $sSQL .="       FROM salas;    ";
+        if(!empty($iIdSala) && $iIdSala > 0){
 
-        $rsSQL = $this->db->query($sSQL);
-        return $rsSQL->result();
+            $this->db->where('id', $iIdSala);
+            $rsQuery = $this->db->get('salas');
+            return $rsQuery->row();
 
-    }
+        }else{
 
-    public function setDeletarSala(){
+            $sSQL  ="     SELECT *              ";
+            $sSQL .="       FROM salas          ";
+            $sSQL .="      WHERE status = 1;    ";
 
-        $aData = array('estado'                      => 2,
-                       'data_atualizacao'            => now());
+            $rsSQL = $this->db->query($sSQL);
+            return $rsSQL->result();
 
-        $this->db->where('id', $this->input->post('salas'));
-        return $this->db->update('salas', $aData);
+        }
+
+
 
     }
 
@@ -33,10 +36,93 @@ class Sala_model extends CI_Model {
 
     }
 
+    public function setSalaDeletar($iIdSala){
+
+        $sSQLVerificar  = "      SELECT count(*) AS total                                    ";
+        $sSQLVerificar .= "        FROM salas                                                ";
+        $sSQLVerificar .= "  INNER JOIN salas_reservas ON salas_reservas.salas_id = salas.id ";
+        $sSQLVerificar .= "       WHERE salas.id = ".$iIdSala.";                             ";
+        $rsSQLVerificar = $this->db->query($sSQLVerificar);
+
+        if($rsSQLVerificar->row()->total == 0){
+
+            $aData = array('status'                      => 2,
+                           'data_atualizacao'            => 'now()');
+
+            $this->db->where('id', $iIdSala);
+            return $this->db->update('salas', $aData);
+
+        }else{
+
+            return false;
+
+        }
+
+    }
+
+    public function setSalaAtualizar(){
+
+        $aData = array('sala'             => $this->input->post('sala'),
+                       'detalhes'         => $this->input->post('detalhes'),
+                       'data_atualizacao' => 'now()');
+
+        $this->db->where('id', $this->input->post('idado'));
+        return $this->db->update('salas', $aData);
+
+    }
+
     public function setReserva(){
 
-        $aData = array('sala'     => $this->input->post('sala'),
-                       'descricao' => $this->input->post('assunto'));
+
+        if(strpos($this->input->post('hora'), 'A')){
+            $sHora = substr($this->input->post('hora'),0,5).":00";
+        }else{
+
+             switch ( substr($this->input->post('hora'),0,2)) {
+                 case '01':
+                     $sNum = '13';
+                     break;
+                case '02':
+                    $sNum = '14';
+                     break;
+                case '03':
+                     $sNum = '15';
+                     break;
+                case '04':
+                     $sNum = '16';
+                     break;
+                case '05':
+                     $sNum = '17';
+                     break;
+                case '06':
+                     $sNum = '18';
+                     break;
+                case '07':
+                     $sNum = '19';
+                     break;
+                case '08':
+                     $sNum = '20';
+                     break;
+                case '09':
+                     $sNum = '21';
+                     break;
+                case '10':
+                     $sNum = '22';
+                     break;
+                 default:
+                     $sNum = '23';
+                     break;
+             }
+
+             $sHora = $sNum.":00:00";
+
+        }
+
+        $aData = array('descricao'   => $this->input->post('assunto'),
+                       'data'        => implode('-', array_reverse(explode('/',$this->input->post('data')))),
+                       'hora'        => $sHora,
+                       'salas_id'    => $this->input->post('idsala'),
+                       'usuarios_id' => $this->input->post('idusuario'));
         return $this->db->insert('salas_reservas', $aData);
 
     }
