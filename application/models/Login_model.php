274fd124e4caed3d9782483e7c65361521c4f5fc
Gilvan Fernandes <gil-f-o@hotmail.com>
2016-12-18 05:06:30 -0200

    Alteração SQL para validação da sessão

--- application/models/Login_model.php
+++ application/models/Login_model.php
@@ -7,9 +7,11 @@ class Login_model extends CI_Model {
 
     public function getValidacao(){
 
-        $sSQLUsuario  = "     SELECT *             ";
-        $sSQLUsuario .= "       FROM usuarios;     ";
-        
+        $sSQLUsuario  = "     SELECT *                                                  ";
+        $sSQLUsuario .= "       FROM usuarios                                           ";
+        $sSQLUsuario .= "      WHERE email = '".$this->input->post('email')."'          ";
+        $sSQLUsuario .= "        AND senha = '".sha1($this->input->post('senha'))."';   ";
+
         $rsQuery = $this->db->query($sSQLUsuario);
         return $rsQuery->result();
 
